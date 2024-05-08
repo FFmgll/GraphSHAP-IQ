0059e93cbad814e6b09757e7e8a7ca0d306bafb1
FFmgll <ffumagalli@techfak.uni-bielefeld.de>
2024-05-08 18:41:42 +0200

    dataset statistics and complexity plots

---
+++ graph_shapiq_experiments/dataset_statistics.py
@@ -0,0 +1,42 @@
+from shapiq.explainer.graph import get_explanation_instances
+from torch_geometric.utils import to_networkx
+import numpy as np
+import pandas as pd
+import os
+
+if __name__ == "__main__":
+    DATASET_NAMES = [
+        "AIDS",
+        "DHFR",
+        "COX2",
+        "BZR",
+        "PROTEINS",
+        "ENZYMES",
+        "MUTAG",
+        "Mutagenicity",
+    ]  # ["AIDS","DHFR","COX2","BZR","PROTEINS", "ENZYMES", "MUTAG", "Mutagenicity"]
+
+
+    for dataset_name in DATASET_NAMES:
+        all_samples_to_explain = get_explanation_instances(dataset_name)
+        num_nodes = {}
+        num_edges = {}
+        avg_node_degree = {}
+        min_node_degree = {}
+        max_node_degree = {}
+
+        for data_id, explain_instance in enumerate(all_samples_to_explain):
+            num_nodes[data_id] = explain_instance.num_nodes
+            g = to_networkx(explain_instance, to_undirected=False)  # converts into graph
+            avg_node_degree[data_id] = np.mean(g.degree())
+            max_node_degree[data_id] = np.max(g.degree())
+            min_node_degree[data_id] = np.min(g.degree())
+            num_edges[data_id] = g.number_of_edges()
+
+        df = pd.DataFrame(index=num_nodes.keys(), data=num_nodes.values())
+        df["avg_node_degree"] = avg_node_degree
+        df["min_node_degree"] = min_node_degree
+        df["max_node_degree"] = max_node_degree
+        df["num_edges"] = num_edges
+        save_path = os.path.join("../results/complexity_analysis/dataset_statistics", dataset_name + ".csv")
+        df.to_csv(save_path)
