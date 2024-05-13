9edd585cb2708bb070f665925570b37cb01a7d1b
paolofraz <paolofraz@gmail.com>
2024-05-13 10:17:41 +0200

    added validated GIN and GAT for Mutagenicity

--- shapiq/explainer/graph/utils.py
+++ shapiq/explainer/graph/utils.py
@@ -9,7 +9,6 @@ from torch_geometric.data import DataLoader
 from shapiq.explainer.graph.graph_datasets import CustomTUDataset
 from shapiq.explainer.graph.graph_models import GNN
 
-
 GRAPH_DATASETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "graph_datasets")
 MODEL_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ckpt", "graph_prediction")
 
@@ -22,11 +21,11 @@ def _compute_baseline_value(x_graph):
 def get_tu_instances(name):
     """Get the instances to explain for the given TU dataset."""
     dataset = CustomTUDataset(
-        root=GRAPH_DATASETS_DIR,
-        name=name,
-        seed=1234,
-        split_sizes=(0.8, 0.1, 0.1),
-    )
+            root=GRAPH_DATASETS_DIR,
+            name=name,
+            seed=1234,
+            split_sizes=(0.8, 0.1, 0.1),
+            )
     loader = DataLoader(dataset, shuffle=False)
     all_samples_to_explain = []
     for data in loader:
@@ -36,17 +35,17 @@ def get_tu_instances(name):
 
 
 def load_graph_model_architecture(
-    model_type: str,
-    dataset_name: str,
-    n_layers: int,
-    hidden: Union[int, bool] = True,
-    node_bias: bool = True,
-    graph_bias: bool = True,
-    dropout: bool = True,
-    batch_norm: bool = True,
-    jumping_knowledge: bool = True,
-    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-) -> tuple[torch.nn.Module, str]:
+        model_type: str,
+        dataset_name: str,
+        n_layers: int,
+        hidden: Union[int, bool] = True,
+        node_bias: bool = True,
+        graph_bias: bool = True,
+        dropout: bool = True,
+        batch_norm: bool = True,
+        jumping_knowledge: bool = True,
+        device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        ) -> tuple[torch.nn.Module, str]:
     """Loads a graph model architecture, whose weights have to be further trained or loaded.
 
     Args:
@@ -54,19 +53,19 @@ def load_graph_model_architecture(
     Returns:
         The loaded model."""
     if dataset_name in [
-        "AIDS",
-        "DHFR",
-        "COX2",
-        "BZR",
-        "MUTAG",
-        "BENZENE",
-        "PROTEINS",
-        "ENZYMES",
-        "Mutagenicity",
-    ]:
+            "AIDS",
+            "DHFR",
+            "COX2",
+            "BZR",
+            "MUTAG",
+            "BENZENE",
+            "PROTEINS",
+            "ENZYMES",
+            "Mutagenicity",
+            ]:
         dataset = CustomTUDataset(
-            root=GRAPH_DATASETS_DIR, name=dataset_name, seed=1234, split_sizes=(0.8, 0.1, 0.1)
-        )
+                root=GRAPH_DATASETS_DIR, name=dataset_name, seed=1234, split_sizes=(0.8, 0.1, 0.1)
+                )
         num_nodes_features = dataset.graphs.num_node_features
         num_classes = dataset.graphs.num_classes
     else:
@@ -94,34 +93,34 @@ def load_graph_model_architecture(
         raise ValueError("Model type not supported.")
 
     model_id = "_".join(
-        [
-            model_type,
-            dataset_name,
-            str(n_layers),
-            str(node_bias),
-            str(graph_bias),
-            str(hidden),
-            str(dropout),
-            str(batch_norm),
-            str(jumping_knowledge),
-        ]
-    )
+            [
+                    model_type,
+                    dataset_name,
+                    str(n_layers),
+                    str(node_bias),
+                    str(graph_bias),
+                    str(hidden),
+                    str(dropout),
+                    str(batch_norm),
+                    str(jumping_knowledge),
+                    ]
+            )
 
     return model, model_id
 
 
 def load_graph_model(
-    model_type: str,
-    dataset_name: str,
-    n_layers: int,
-    hidden: Union[int, bool] = True,
-    node_bias: bool = True,
-    graph_bias: bool = True,
-    dropout: bool = True,
-    batch_norm: bool = True,
-    jumping_knowledge: bool = True,
-    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-) -> torch.nn.Module:
+        model_type: str,
+        dataset_name: str,
+        n_layers: int,
+        hidden: Union[int, bool] = True,
+        node_bias: bool = True,
+        graph_bias: bool = True,
+        dropout: bool = True,
+        batch_norm: bool = True,
+        jumping_knowledge: bool = True,
+        device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        ) -> torch.nn.Module:
     """Loads a pre-trained graph model from disk with the given configuration.
 
     Args:
@@ -143,17 +142,17 @@ def load_graph_model(
     """
     try:
         model, model_id = load_graph_model_architecture(
-            model_type=model_type,
-            dataset_name=dataset_name,
-            n_layers=n_layers,
-            hidden=hidden,
-            node_bias=node_bias,
-            graph_bias=graph_bias,
-            dropout=dropout,
-            batch_norm=batch_norm,
-            jumping_knowledge=jumping_knowledge,
-            device=device,
-        )
+                model_type=model_type,
+                dataset_name=dataset_name,
+                n_layers=n_layers,
+                hidden=hidden,
+                node_bias=node_bias,
+                graph_bias=graph_bias,
+                dropout=dropout,
+                batch_norm=batch_norm,
+                jumping_knowledge=jumping_knowledge,
+                device=device,
+                )
 
         # Construct the path to the target directory
         target_dir = os.path.join(MODEL_DIR, model_type, dataset_name)
@@ -170,15 +169,15 @@ def load_graph_model(
 def get_explanation_instances(dataset_name):
     """Get the instances to explain for the given dataset."""
     if dataset_name in [
-        "AIDS",
-        "DHFR",
-        "COX2",
-        "BZR",
-        "PROTEINS",
-        "ENZYMES",
-        "MUTAG",
-        "Mutagenicity",
-    ]:
+            "AIDS",
+            "DHFR",
+            "COX2",
+            "BZR",
+            "PROTEINS",
+            "ENZYMES",
+            "MUTAG",
+            "Mutagenicity",
+            ]:
         all_samples_to_explain = get_tu_instances(dataset_name)
         return all_samples_to_explain
     raise ValueError("Dataset not supported.")
@@ -186,50 +185,69 @@ def get_explanation_instances(dataset_name):
 
 # Helper container with all the stored best configurations
 _best_hyperparameters = {
-    "GCN": {
-        "AIDS": {
-            "n_layers": {
-                "1": {"hidden": 128},
-                "2": {"hidden": 128},
-                "3": {"hidden": 128},
-                "4": {"hidden": 128},
-            }
-        },
-        "DHFR": {
-            "n_layers": {
-                "1": {"hidden": 64},
-                "2": {"hidden": 128},
-                "3": {"hidden": 32},
-                "4": {"hidden": 64},
-            }
-        },
-        "COX2": {
-            "n_layers": {
-                "1": {"hidden": 128},
-                "2": {"hidden": 128},
-                "3": {"hidden": 128},
-                "4": {"hidden": 128},
-            }
-        },
-        "BZR": {
-            "n_layers": {
-                "1": {"hidden": 128},
-                "2": {"hidden": 32},
-                "3": {"hidden": 64},
-                "4": {"hidden": 64},
-            },
-        },
-        "PROTEINS": {},
-        "ENZYMES": {},
-        "MUTAG": {},
-        "Mutagenicity": {
-            "n_layers": {
-                "1": {"hidden": 128},
-                "2": {"hidden": 128},
-                "3": {"hidden": 128},
-                "4": {"hidden": 64},
-            }
-        },
-    },
-    "GIN": {},
-}
+        "GCN": {
+                "AIDS": {
+                        "n_layers": {
+                                "1": {"hidden": 128},
+                                "2": {"hidden": 128},
+                                "3": {"hidden": 128},
+                                "4": {"hidden": 128},
+                                }
+                        },
+                "DHFR": {
+                        "n_layers": {
+                                "1": {"hidden": 64},
+                                "2": {"hidden": 128},
+                                "3": {"hidden": 32},
+                                "4": {"hidden": 64},
+                                }
+                        },
+                "COX2": {
+                        "n_layers": {
+                                "1": {"hidden": 128},
+                                "2": {"hidden": 128},
+                                "3": {"hidden": 128},
+                                "4": {"hidden": 128},
+                                }
+                        },
+                "BZR": {
+                        "n_layers": {
+                                "1": {"hidden": 128},
+                                "2": {"hidden": 32},
+                                "3": {"hidden": 64},
+                                "4": {"hidden": 64},
+                                },
+                        },
+                "PROTEINS": {},
+                "ENZYMES": {},
+                "MUTAG": {},
+                "Mutagenicity": {
+                        "n_layers": {
+                                "1": {"hidden": 128},
+                                "2": {"hidden": 128},
+                                "3": {"hidden": 128},
+                                "4": {"hidden": 64},
+                                }
+                        },
+                },
+        "GIN": {
+                "Mutagenicity": {
+                        "n_layers": {
+                                "1": {"hidden": 128},
+                                "2": {"hidden": 32},
+                                "3": {"hidden": 32},
+                                "4": {"hidden": 64},
+                                },
+                        },
+                "GAT": {
+                        "Mutagenicity": {
+                                "n_layers": {
+                                        "1": {"hidden": 128},
+                                        "2": {"hidden": 64},
+                                        "3": {"hidden": 128},
+                                        "4": {"hidden": 64},
+                                        }
+                                },
+                        },
+                },
+        }
