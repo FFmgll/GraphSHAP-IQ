f5bfe9254687f83efe07f628b0c2bdaa48b46270
FFmgll <ffumagalli@techfak.uni-bielefeld.de>
2024-11-14 18:48:26 +0100

    added R2 of logarithmic curve to complexity plot

--- graph_shapiq_experiments/complexity_plot.py
+++ graph_shapiq_experiments/complexity_plot.py
@@ -26,7 +26,9 @@ def plot_trend_curve(x_values, values, color, type="log"):
     if type == "log":
         popt, pcov = curve_fit(logarithmic_function, x_val, y_val)
         y_fit = logarithmic_function(x_val, *popt)
-        #
+
+        # R-squared
+
         idx_to_plot = y_fit < x_val * np.log10(2)
         plot_y = y_fit[idx_to_plot]
         plot_x = x_val[idx_to_plot]
@@ -42,14 +44,28 @@ def plot_trend_curve(x_values, values, color, type="log"):
     if type == "lin":
         coefficients = np.polyfit(x_val, y_val, 1)
         polynomial_func = np.poly1d(coefficients)
+        y_fit = polynomial_func(x_val)
         plt.plot(
             x_val,
-            polynomial_func(x_val),
+            y_fit,
             color=color,
             linestyle=":",
             linewidth=1,
             label="",
         )
+
+    def calculate_r_squared(y_val, y_fit):
+        ss_res = np.sum(residuals**2)
+        ss_tot = np.sum((y_val - np.mean(y_val)) ** 2)
+        r_squared = 1 - (ss_res / ss_tot)
+        return r_squared
+
+        # Calculate metrics
+
+    residuals = y_val - y_fit
+    r_squared = calculate_r_squared(y_val, y_fit)
+
+    return r_squared
 
 
 def plot_naive_budget(node_range):
@@ -90,7 +106,9 @@ def plot_complexity_by_layers(plot_dataset, dataset, scatter=False):
 
         if scatter:
             try:
-                plot_trend_curve(plot_medians.index, plot_medians, plot_color, type="log")
+                r_squared = plot_trend_curve(
+                    plot_medians.index, plot_medians, plot_color, type="log"
+                )
             except:
                 print("No trend curve fitted")
             # Plot the median as line plot
@@ -100,11 +118,13 @@ def plot_complexity_by_layers(plot_dataset, dataset, scatter=False):
                 color=plot_color,
                 s=5,
                 marker="s",
-                label=plot_label,
+                label=plot_label+ ' ('+str(np.round(r_squared,2))+")",
             )
         else:
             try:
-                plot_trend_curve(plot_medians.index, plot_medians, plot_color, type="log")
+                r_squared = plot_trend_curve(
+                    plot_medians.index, plot_medians, plot_color, type="log"
+                )
             except:
                 print("No trend curve fitted")
             # Plot the median as line plot
@@ -113,7 +133,7 @@ def plot_complexity_by_layers(plot_dataset, dataset, scatter=False):
                 plot_medians,
                 color=plot_color,
                 linestyle="-",
-                label=plot_label,
+                label=plot_label+ ' ('+str(np.round(r_squared,2))+")",
             )
             # Plot the band around median using Q1 and Q3
             ax.fill_between(
@@ -123,6 +143,7 @@ def plot_complexity_by_layers(plot_dataset, dataset, scatter=False):
                 alpha=0.2,
                 color=plot_color,
             )
+
 
     # Axis customization
     min_x = plot_dataset["n_players"].min() - 2
@@ -146,16 +167,31 @@ def plot_complexity_by_layers(plot_dataset, dataset, scatter=False):
     plt.grid(True, linestyle=":")
     plt.tight_layout()
     if scatter:
-        plt.savefig(os.path.join(save_path_plots, "complexity_by_layers_" + dataset + ".png"))
+        plt.savefig(
+            os.path.join(save_path_plots, "complexity_by_layers_" + dataset + ".png")
+        )
     else:
         plt.savefig(
-            os.path.join(save_path_plots, "complexity_by_layers_" + dataset + "_median_q1_q3.png")
+            os.path.join(
+                save_path_plots, "complexity_by_layers_" + dataset + "_median_q1_q3.png"
+            )
         )
     plt.show()
 
 
 def plot_complexity_by_statistic(
-    save_id, plot_dataset, statistic, title, clabel, min_x, max_x, min_v, max_v, min_y, max_y, cmap
+    save_id,
+    plot_dataset,
+    statistic,
+    title,
+    clabel,
+    min_x,
+    max_x,
+    min_v,
+    max_v,
+    min_y,
+    max_y,
+    cmap,
 ):
     plt.rcParams.update(params)
 
@@ -201,17 +237,17 @@ if __name__ == "__main__":
 
     COLOR_LIST = ["#ef27a6", "#7d53de", "#00b4d8", "#ff6f00", "#ffba08"]
     params = {
-        #"legend.fontsize": "x-large",
+        # "legend.fontsize": "x-large",
         "figure.figsize": (8, 7),
-        #"axes.labelsize": "x-large",
-        #"axes.titlesize": "x-large",
-        #"xtick.labelsize": "x-large",
-        #"ytick.labelsize": "x-large",
-        "font.size": 20
+        # "axes.labelsize": "x-large",
+        # "axes.titlesize": "x-large",
+        # "xtick.labelsize": "x-large",
+        # "ytick.labelsize": "x-large",
+        "font.size": 20,
     }
 
-    #plt.rcParams.update({"font.size": 20})  # increase the font size of the plot
-    #plt.rcParams["figure.figsize"] = (8, 7)  # set figure size
+    # plt.rcParams.update({"font.size": 20})  # increase the font size of the plot
+    # plt.rcParams["figure.figsize"] = (8, 7)  # set figure size
 
     DATASETS = [
         "COX2",
@@ -222,14 +258,16 @@ if __name__ == "__main__":
         "FluorideCarbonyl",
         "Benzene",
         "AlkaneCarbonyl",
-        "WaterQuality"
+        "WaterQuality",
     ]
 
     LAYERS = ["1", "2", "3", "4"]
 
     # Import dataset statistics
     dataset_statistics = {}
-    for file_path in glob.glob(os.path.join(save_directory, "dataset_statistics", "*.csv")):
+    for file_path in glob.glob(
+        os.path.join(save_directory, "dataset_statistics", "*.csv")
+    ):
         dataset_statistic = pd.read_csv(file_path)
         dataset_name = file_path.split("/")[-1][:-4]
         if dataset_name in DATASETS:
@@ -252,7 +290,9 @@ if __name__ == "__main__":
                 )
                 results[file_name] = result
 
-    all_datasets = pd.concat(dataset_statistics.values(), keys=dataset_statistics.keys())
+    all_datasets = pd.concat(
+        dataset_statistics.values(), keys=dataset_statistics.keys()
+    )
 
     # Rename unnamed index column and introduce dataset_name column
     all_datasets["dataset_name"] = all_datasets.index.get_level_values(0)
@@ -260,28 +300,43 @@ if __name__ == "__main__":
     dataset_counts = all_datasets.groupby(["dataset_name"]).count()
     dataset_means = all_datasets.groupby(["dataset_name"]).mean()
     # Round and transform to percentages for better readability
-    dataset_means["avg_num_nodes"] =  np.round(dataset_means["0"],2)
-    dataset_means["avg_graph_density"] = np.round(dataset_means["graph_density"]*100,2)
+    dataset_means["avg_num_nodes"] = np.round(dataset_means["0"], 2)
+    dataset_means["avg_graph_density"] = np.round(
+        dataset_means["graph_density"] * 100, 2
+    )
 
     df = pd.concat(results.values(), keys=results.keys())
     df["log10_budget"] = np.log10(df["budget"].astype(float))
-    df["log10_budget_capped"] = df["log10_budget"].clip(upper=df["n_players"] * np.log10(2))
+    df["log10_budget_capped"] = df["log10_budget"].clip(
+        upper=df["n_players"] * np.log10(2)
+    )
 
     means = df.groupby(["dataset_name", "n_layers", "n_players"])["log10_budget"].mean()
-    q1 = df.groupby(["dataset_name", "n_layers", "n_players"])["log10_budget"].quantile(0.25)
-    q3 = df.groupby(["dataset_name", "n_layers", "n_players"])["log10_budget"].quantile(0.75)
-    medians = df.groupby(["dataset_name", "n_layers", "n_players"])["log10_budget"].median()
+    q1 = df.groupby(["dataset_name", "n_layers", "n_players"])["log10_budget"].quantile(
+        0.25
+    )
+    q3 = df.groupby(["dataset_name", "n_layers", "n_players"])["log10_budget"].quantile(
+        0.75
+    )
+    medians = df.groupby(["dataset_name", "n_layers", "n_players"])[
+        "log10_budget"
+    ].median()
     stds = df.groupby(["dataset_name", "n_layers", "n_players"])["log10_budget"].std()
 
-
     # Set global display format to suppress scientific notation
-    pd.options.display.float_format = '{:.0e}'.format
+    pd.options.display.float_format = "{:.0e}".format
 
     # We compute budget ratios in log-scale for numerical stability and report median of those values
-    df["budget_ratio_perc"] = np.exp((np.log(df["budget"].astype(float)) - df["n_players"]*np.log(2)))*100
-    budget_ratio_perc_median = np.round(df.groupby(["dataset_name", "n_layers"])["budget_ratio_perc"].median(),4)
+    df["budget_ratio_perc"] = (
+        np.exp((np.log(df["budget"].astype(float)) - df["n_players"] * np.log(2))) * 100
+    )
+    budget_ratio_perc_median = np.round(
+        df.groupby(["dataset_name", "n_layers"])["budget_ratio_perc"].median(), 4
+    )
     # Compute the multiplier reported in Table 1.
-    budget_speedup_multiplier = np.round(100/df.groupby(["dataset_name", "n_layers"])["budget_ratio_perc"].median(),0)
+    budget_speedup_multiplier = np.round(
+        100 / df.groupby(["dataset_name", "n_layers"])["budget_ratio_perc"].median(), 0
+    )
 
     for dataset in df["dataset_name"].unique():
         if dataset != "WaterQuality":
@@ -294,19 +349,19 @@ if __name__ == "__main__":
     # Graph Density Plot
     dataset_name = "Mutagenicity"
     n_layers = "2"
+    plot_dataet = df.copy()
     plot_dataset = df[df["n_layers"] == n_layers]
     plot_dataset = plot_dataset[plot_dataset["dataset_name"] == dataset_name]
-    plot_dataset["graph_density"] *= 100
-    statistic = "graph_density"
     save_id = "complexity_by_graph_density_" + dataset_name + "_" + n_layers
     min_x = max(plot_dataset["n_players"].min(), 5)
     max_x = min(plot_dataset["n_players"].max(), 65)
-    min_v = plot_dataset[statistic].quantile(0.1)
-    max_v = plot_dataset[statistic].quantile(0.9)
+    min_v = 0.05
+    max_v = 0.55
     min_y = 1.5
     max_y = 6.5
-    clabel = "Graph Density (%)"
-    title = "Mutagenicity (2-Layer GNN)"#"Exact Shapley Explanations on Mutagenicity (2-Layer GNN)"
+    statistic = "graph_density"
+    clabel = "Graph Density"
+    title = "Mutagenicity (2-Layer GNN)"  # "Exact Shapley Explanations on Mutagenicity (2-Layer GNN)"
     plot_complexity_by_statistic(
         save_id,
         plot_dataset,
@@ -322,22 +377,20 @@ if __name__ == "__main__":
         cmap="plasma",
     )
 
-
-    # Bound Plot
+    # Graph Density Plot
     dataset_name = "Mutagenicity"
     n_layers = "2"
+    plot_dataet = df.copy()
     plot_dataset = df[df["n_layers"] == n_layers]
     plot_dataset = plot_dataset[plot_dataset["dataset_name"] == dataset_name]
-    plot_dataset["bound"] = np.exp(plot_dataset["max_node_degree"]**plot_dataset["n_layers"].astype("int"))/plot_dataset["n_players"]
-
-    save_id = "complexity_by_bound_" + dataset_name + "_" + n_layers
-    statistic = "bound"
+    save_id = "complexity_by_max_degree_" + dataset_name + "_" + n_layers
     min_x = max(plot_dataset["n_players"].min(), 5)
     max_x = min(plot_dataset["n_players"].max(), 65)
-    min_v = plot_dataset[statistic].quantile(0.05)
-    max_v = plot_dataset[statistic].quantile(0.95)
+    min_v = 10
+    max_v = 65
     min_y = 1.5
     max_y = 6.5
+    statistic = "max_node_degree"
     clabel = "Maximum Node Degree"
     title = "Exact Shapley Explanations on Mutagenicity (2-Layer GNN)"
     plot_complexity_by_statistic(
